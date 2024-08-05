6bda24984fec8bac621f459a39f903889afd039b
Thomas Prescher <thomas.prescher@cyberus-technology.de>
2024-08-05 10:59:21 +0200

    XXX: fill registers with magic values

--- src/tests/timing/main.cpp
+++ src/tests/timing/main.cpp
@@ -6,6 +6,7 @@
 #include <toyos/baretest/baretest.hpp>
 #include <toyos/x86/x86asm.hpp>
 
+#if 0
 TEST_CASE(tsc_is_monotonous)
 {
     // Run for at least some minutes.
@@ -35,5 +36,50 @@ TEST_CASE(tsc_is_monotonous)
         BARETEST_ASSERT(tsc_before < tsc_after);
     }
 }
+#endif
+
+TEST_CASE(register_fill)
+{
+    uint64_t* mem_ptr_0 = (uint64_t*)0x0;
+    uint64_t* mem_ptr = (uint64_t*)0x1000;
+
+    //*mem_ptr_0 = 0x00c0fefe13381338;
+
+    for (int i = 0; i < 1000; ++i) {
+	*(mem_ptr + i) = 0xdeadbeef13371337ull;
+    }
+
+
+    uint64_t cr0 {get_cr0()};
+    uint64_t cr3 {get_cr3()};
+    uint64_t cr4 {get_cr4()};
+    auto gdtr {get_current_gdtr()};
+    auto idtr {get_current_idtr()};
+    info("CR0:{x} CR3:{x} CR4:{x} GDTR:{x}+{x} IDTR:{x}+{x} {x}", cr0, cr3, cr4, gdtr.base, gdtr.limit, idtr.base, idtr.limit, *mem_ptr_0);
+    //*mem_ptr_0 = 0x00c0fefe13381338;
+    asm volatile(
+	    "push %%rbp;"
+	    "mov $0xa0a0a0a0a0a0a0a0, %%rax;"
+	    "mov %%rax, %%cr2;"
+	    "mov $0x1111111111111111, %%rax;"
+	    "mov $0x2222222222222222, %%rbx;"
+	    "mov $0x3333333333333333, %%rcx;"
+	    "mov $0x4444444444444444, %%rdx;"
+	    "mov $0x5555555555555555, %%rsi;"
+	    "mov $0x6666666666666666, %%rdi;"
+	    "mov $0xb1b1b1b1b1b1b1b1, %%r8; "
+	    "mov $0x7777777777777777, %%r9; "
+	    "mov $0x8888888888888888, %%r10;"
+	    "mov $0x9999999999999999, %%r11;"
+	    "mov $0xaaaaaaaaaaaaaaaa, %%r12;"
+	    "mov $0xbbbbbbbbbbbbbbbb, %%r13;"
+	    "mov $0xcccccccccccccccc, %%r14;"
+	    "mov $0xdddddddddddddddd, %%r15;"
+	    "mov $0xeeeeeeeeeeeeeeee, %%rbp;"
+	    "mov $0xc2c2c2c2c2c2c2c2, %%rsp;"
+	    "jmp .;"
+	    ::: "memory");
+}
+
 
 BARETEST_RUN;
